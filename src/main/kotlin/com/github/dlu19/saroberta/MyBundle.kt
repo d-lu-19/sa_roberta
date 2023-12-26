4e8e8b03113b592d41541e02a7048101b6463479
GitHub Action <action@github.com>
2023-12-26 13:04:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/dlu19/saroberta/MyBundle.kt
+++ src/main/kotlin/com/github/dlu19/saroberta/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dlu19.saroberta
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dlu19/saroberta/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dlu19/saroberta/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.dlu19.saroberta.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dlu19.saroberta.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
